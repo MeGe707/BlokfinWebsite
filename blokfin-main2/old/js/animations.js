ed3e2e7ccfa765c0a45e5b55e8fb538234f213d5
MeGe707 <mehmet0707gul@gmail.com>
2022-03-28 21:29:49 +0300

    2

--- blokfin-main2/old/js/animations.js
+++ blokfin-main2/old/js/animations.js
@@ -7,9 +7,9 @@ function animation () {
   const t1 = gsap.timeline({ defaults: { ease: Expo.InOut } });
 
   t1.fromTo(cards1Text, 2, {opacity: 0}, { opacity: 1 });
-  t1.fromTo(".card-1-image-1", 2, {x: "-150rem"}, {x: "0rem"}, "-=2");
-  t1.fromTo(".card-1-image-2", 2, {opacity: 0}, {opacity: 1}, "-=0");
-  t1.fromTo(".card-1-image-3", 2, {x: "700rem"}, {x: "0rem"}, "-=4");
+  t1.fromTo(".card-1-image-1", 2, {x: "-7rem", y: "10rem", opacity: 0}, {x: "0rem", y: "0rem", opacity:1 }, "-=2");
+  t1.fromTo(".card-1-image-2", 2, {x: "-7rem", y: "10rem", opacity: 0}, {x: "0rem", y: "0rem", opacity:1 }, "-=2");
+  t1.fromTo(".card-1-image-3", 2, {x: "-7rem", y: "10rem", opacity: 0}, {x: "0rem", y: "0rem", opacity:1 }, "-=2");
 
 
     new ScrollMagic.Scene({
@@ -21,9 +21,7 @@ function animation () {
     .addTo(controller);
 }
 
-  const t2 = gsap.timeline({ defaults: { ease: Expo.InOut } });
 
-  t2.fromTo(".robot", 2, {opacity: 0, x: "500rem", y: "-100rem"}, { opacity: 1, x: "0", y: "0rem" }, );
 
   const t3 = gsap.timeline({ defaults: { ease: Expo.InOut } });
 
