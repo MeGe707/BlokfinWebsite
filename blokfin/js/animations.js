1e8cc0f191c05e195150ec412294525ae732751a
MeGe707 <mehmet0707gul@gmail.com>
2022-07-15 11:29:41 +0300

    update

---
+++ blokfin/js/animations.js
@@ -0,0 +1,42 @@
+const cards1Text = document.querySelector(".card-1-text");
+
+function animation () {
+
+  var controller = new ScrollMagic.Controller();
+
+  const t1 = gsap.timeline({ defaults: { ease: Expo.InOut } });
+
+  t1.fromTo(cards1Text, 2, {opacity: 0}, { opacity: 1 }, );
+  t1.fromTo(".card-1-image-1", 1, {x: "-7rem", y: "10rem", opacity: 0}, {x: "0rem", y: "0rem", opacity:1 }, "-=2");
+  t1.fromTo(".card-1-image-2", 1, {x: "-7rem", y: "10rem", opacity: 0}, {x: "0rem", y: "0rem", opacity:1 }, "-=2");
+  t1.fromTo(".card-1-image-3", 1, {x: "-7rem", y: "10rem", opacity: 0}, {x: "0rem", y: "0rem", opacity:1 }, "-=2");
+
+
+    new ScrollMagic.Scene({
+    triggerElement: ".scroll-1",
+    triggerHook: 0.3,
+    reverse: false,
+  })
+    .setTween(t1)
+    .addTo(controller);
+}
+
+
+
+  const t3 = gsap.timeline({ defaults: { ease: Expo.InOut } });
+
+  t3.fromTo(".header-text", 3, {opacity: 0}, { opacity: 1 }, );
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+animation();
---
+++ blokfin/js/myCodes.js
@@ -0,0 +1,810 @@
+let code1 = document.getElementById("code1");
+let dots1 = document.getElementById("dots1");
+let code2 = document.getElementById("code2");
+let dots2 = document.getElementById("dots2");
+let code3 = document.getElementById("code3");
+let dots3 = document.getElementById("dots3");
+let code4 = document.getElementById("code4");
+let dots4 = document.getElementById("dots4");
+let code5 = document.getElementById("code5");
+let dots5 = document.getElementById("dots5");
+let code6 = document.getElementById("code6");
+let dots6 = document.getElementById("dots6");
+let code7 = document.getElementById("code7");
+let dots7 = document.getElementById("dots7");
+let code8 = document.getElementById("code8");
+let dots8 = document.getElementById("dots8");
+let code9 = document.getElementById("code9");
+let dots9 = document.getElementById("dots9");
+let code10 = document.getElementById("code10");
+let dots10 = document.getElementById("dots10");
+let code11 = document.getElementById("code11");
+let dots11 = document.getElementById("dots11");
+let code12 = document.getElementById("code12");
+let dots12 = document.getElementById("dots12");
+let code13 = document.getElementById("code13");
+let dots13 = document.getElementById("dots13");
+let code14 = document.getElementById("code14");
+let dots14 = document.getElementById("dots14");
+let code15 = document.getElementById("code15");
+let dots15 = document.getElementById("dots15");
+let code16 = document.getElementById("code16");
+let dots16 = document.getElementById("dots16");
+let code17 = document.getElementById("code1");
+let dots17 = document.getElementById("dots1");
+let code18 = document.getElementById("code18");
+let dots18 = document.getElementById("dots18");
+let code19 = document.getElementById("code19");
+let dots19 = document.getElementById("dots19");
+let code20 = document.getElementById("code20");
+let dots20 = document.getElementById("dots20");
+let code21 = document.getElementById("code21");
+let dots21 = document.getElementById("dots21");
+let code22 = document.getElementById("code22");
+let dots22 = document.getElementById("dots22");
+let code23 = document.getElementById("code23");
+let dots23 = document.getElementById("dots23");
+let code24 = document.getElementById("code24");
+let dots24 = document.getElementById("dots24");
+let code25 = document.getElementById("code25");
+let dots25 = document.getElementById("dots25");
+let code26 = document.getElementById("code26");
+let dots26 = document.getElementById("dots26");
+let code27 = document.getElementById("code27");
+let dots27 = document.getElementById("dots27");
+let code28 = document.getElementById("code28");
+let dots28 = document.getElementById("dots28");
+let code29 = document.getElementById("code29");
+let dots29 = document.getElementById("dots29");
+let code30 = document.getElementById("code30");
+let dots30 = document.getElementById("dots30");
+let code31 = document.getElementById("code31");
+let dots31 = document.getElementById("dots31");
+let code32 = document.getElementById("code32");
+let dots32 = document.getElementById("dots32");
+
+
+let dots = document.querySelector(".dots")
+
+
+
+
+function dots1up(){
+    dots1.style.opacity = '1'
+
+}
+
+function dots1down(){
+    dots1.style.opacity = '0'
+}
+
+function dots1purple(){
+
+    dots1.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots1.style.color = "white";
+}
+
+function dots1white(){
+    dots1.style.backgroundColor = "white";
+    dots1.style.color = 'rgba(84,84,85,255)';
+}
+
+
+
+
+
+
+function dots2up(){
+    dots2.style.opacity = '1'
+
+}
+
+function dots2down(){
+    dots2.style.opacity = '0'
+}
+
+function dots2purple(){
+
+    dots2.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots2.style.color = "white";
+}
+
+function dots2white(){
+    dots2.style.backgroundColor = "white";
+    dots2.style.color = 'rgba(84,84,85,255)';
+}
+
+
+
+
+
+
+
+function dots3up(){
+    dots3.style.opacity = '1'
+
+}
+
+function dots3down(){
+    dots3.style.opacity = '0'
+}
+
+function dots3purple(){
+
+    dots3.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots3.style.color = "white";
+}
+
+function dots3white(){
+    dots3.style.backgroundColor = "white";
+    dots3.style.color = 'rgba(84,84,85,255)';
+}
+
+
+
+
+
+
+
+
+
+function dots4up(){
+    dots4.style.opacity = '1'
+
+}
+
+function dots4down(){
+    dots4.style.opacity = '0'
+}
+
+function dots4purple(){
+
+    dots4.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots4.style.color = "white";
+}
+
+function dots4white(){
+    dots4.style.backgroundColor = "white";
+    dots4.style.color = 'rgba(84,84,85,255)';
+}
+
+
+
+
+
+
+
+function dots5up(){
+    dots5.style.opacity = '1'
+
+}
+
+function dots5down(){
+    dots5.style.opacity = '0'
+}
+
+function dots5purple(){
+
+    dots5.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots5.style.color = "white";
+}
+
+function dots5white(){
+    dots5.style.backgroundColor = "white";
+    dots5.style.color = 'rgba(84,84,85,255)';
+}
+
+
+
+
+
+
+function dots6up(){
+    dots6.style.opacity = '1'
+
+}
+
+function dots6down(){
+    dots6.style.opacity = '0'
+}
+
+function dots6purple(){
+
+    dots6.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots6.style.color = "white";
+}
+
+function dots6white(){
+    dots6.style.backgroundColor = "white";
+    dots6.style.color = 'rgba(84,84,85,255)';
+}
+
+
+
+
+
+
+
+function dots7up(){
+    dots7.style.opacity = '1'
+
+}
+
+function dots7down(){
+    dots7.style.opacity = '0'
+}
+
+function dots7purple(){
+
+    dots7.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots7.style.color = "white";
+}
+
+function dots7white(){
+    dots7.style.backgroundColor = "white";
+    dots7.style.color = 'rgba(84,84,85,255)';
+}
+
+
+
+
+
+
+
+
+
+function dots8up(){
+    dots8.style.opacity = '1'
+
+}
+
+function dots8down(){
+    dots8.style.opacity = '0'
+}
+
+function dots8purple(){
+
+    dots8.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots8.style.color = "white";
+}
+
+function dots8white(){
+    dots8.style.backgroundColor = "white";
+    dots8.style.color = 'rgba(84,84,85,255)';
+}
+
+
+
+
+
+
+
+function dots9up(){
+    dots9.style.opacity = '1'
+
+}
+
+function dots9down(){
+    dots9.style.opacity = '0'
+}
+
+function dots9purple(){
+
+    dots9.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots9.style.color = "white";
+}
+
+function dots9white(){
+    dots9.style.backgroundColor = "white";
+    dots9.style.color = 'rgba(84,84,85,255)';
+}
+
+
+
+
+
+
+function dots10up(){
+    dots10.style.opacity = '1'
+
+}
+
+function dots10down(){
+    dots10.style.opacity = '0'
+}
+
+function dots10purple(){
+
+    dots10.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots10.style.color = "white";
+}
+
+function dots10white(){
+    dots10.style.backgroundColor = "white";
+    dots10.style.color = 'rgba(84,84,85,255)';
+}
+
+
+
+
+
+
+
+function dots11up(){
+    dots11.style.opacity = '1'
+
+}
+
+function dots11down(){
+    dots11.style.opacity = '0'
+}
+
+function dots11purple(){
+
+    dots11.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots11.style.color = "white";
+}
+
+function dots11white(){
+    dots11.style.backgroundColor = "white";
+    dots11.style.color = 'rgba(84,84,85,255)';
+}
+
+
+
+
+
+
+
+
+
+function dots12up(){
+    dots12.style.opacity = '1'
+
+}
+
+function dots12down(){
+    dots12.style.opacity = '0'
+}
+
+function dots12purple(){
+
+    dots12.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots12.style.color = "white";
+}
+
+function dots12white(){
+    dots12.style.backgroundColor = "white";
+    dots12.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots13up(){
+    dots13.style.opacity = '1'
+
+}
+
+function dots13down(){
+    dots13.style.opacity = '0'
+}
+
+function dots13purple(){
+
+    dots13.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots13.style.color = "white";
+}
+
+function dots13white(){
+    dots13.style.backgroundColor = "white";
+    dots13.style.color = 'rgba(84,84,85,255)';
+}
+
+
+function dots14up(){
+    dots14.style.opacity = '1'
+
+}
+
+function dots14down(){
+    dots14.style.opacity = '0'
+}
+
+function dots14purple(){
+
+    dots14.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots14.style.color = "white";
+}
+
+function dots14white(){
+    dots14.style.backgroundColor = "white";
+    dots14.style.color = 'rgba(84,84,85,255)';
+}
+
+
+function dots15up(){
+    dots15.style.opacity = '1'
+
+}
+
+function dots15down(){
+    dots15.style.opacity = '0'
+}
+
+function dots15purple(){
+
+    dots15.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots15.style.color = "white";
+}
+
+function dots15white(){
+    dots15.style.backgroundColor = "white";
+    dots15.style.color = 'rgba(84,84,85,255)';
+}
+
+
+function dots16up(){
+    dots16.style.opacity = '1'
+
+}
+
+function dots16down(){
+    dots16.style.opacity = '0'
+}
+
+function dots16purple(){
+
+    dots16.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots16.style.color = "white";
+}
+
+function dots16white(){
+    dots16.style.backgroundColor = "white";
+    dots16.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots17up(){
+    dots17.style.opacity = '1'
+
+}
+
+function dots17down(){
+    dots17.style.opacity = '0'
+}
+
+function dots17purple(){
+
+    dots17.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots17.style.color = "white";
+}
+
+function dots17white(){
+    dots17.style.backgroundColor = "white";
+    dots17.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots18up(){
+    dots18.style.opacity = '1'
+
+}
+
+function dots18down(){
+    dots18.style.opacity = '0'
+}
+
+function dots18purple(){
+
+    dots18.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots18.style.color = "white";
+}
+
+function dots18white(){
+    dots18.style.backgroundColor = "white";
+    dots18.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots19up(){
+    dots19.style.opacity = '1'
+
+}
+
+function dots19down(){
+    dots19.style.opacity = '0'
+}
+
+function dots19purple(){
+
+    dots19.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots19.style.color = "white";
+}
+
+function dots19white(){
+    dots19.style.backgroundColor = "white";
+    dots19.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots20up(){
+    dots20.style.opacity = '1'
+
+}
+
+function dots20down(){
+    dots20.style.opacity = '0'
+}
+
+function dots20purple(){
+
+    dots20.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots20.style.color = "white";
+}
+
+function dots20white(){
+    dots20.style.backgroundColor = "white";
+    dots20.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots21up(){
+    dots21.style.opacity = '1'
+
+}
+
+function dots21down(){
+    dots21.style.opacity = '0'
+}
+
+function dots21purple(){
+
+    dots21.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots21.style.color = "white";
+}
+
+function dots21white(){
+    dots21.style.backgroundColor = "white";
+    dots21.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots22up(){
+    dots22.style.opacity = '1'
+
+}
+
+function dots22down(){
+    dots22.style.opacity = '0'
+}
+
+function dots22purple(){
+
+    dots22.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots22.style.color = "white";
+}
+
+function dots22white(){
+    dots22.style.backgroundColor = "white";
+    dots22.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots23up(){
+    dots23.style.opacity = '1'
+
+}
+
+function dots23down(){
+    dots23.style.opacity = '0'
+}
+
+function dots23purple(){
+
+    dots23.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots23.style.color = "white";
+}
+
+function dots24white(){
+    dots24.style.backgroundColor = "white";
+    dots24.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots25up(){
+    dots25.style.opacity = '1'
+
+}
+
+function dots25down(){
+    dots25.style.opacity = '0'
+}
+
+function dots25purple(){
+
+    dots25.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots25.style.color = "white";
+}
+
+function dots25white(){
+    dots25.style.backgroundColor = "white";
+    dots25.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots26up(){
+    dots26.style.opacity = '1'
+
+}
+
+function dots26down(){
+    dots26.style.opacity = '0'
+}
+
+function dots26purple(){
+
+    dots26.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots26.style.color = "white";
+}
+
+function dots26white(){
+    dots26.style.backgroundColor = "white";
+    dots26.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots27up(){
+    dots27.style.opacity = '1'
+
+}
+
+function dots27down(){
+    dots27.style.opacity = '0'
+}
+
+function dots27purple(){
+
+    dots27.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots27.style.color = "white";
+}
+
+function dots27white(){
+    dots27.style.backgroundColor = "white";
+    dots27.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots28up(){
+    dots28.style.opacity = '1'
+
+}
+
+function dots28down(){
+    dots28.style.opacity = '0'
+}
+
+function dots28purple(){
+
+    dots28.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots28.style.color = "white";
+}
+
+function dots20white(){
+    dots28.style.backgroundColor = "white";
+    dots28.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots29up(){
+    dots29.style.opacity = '1'
+
+}
+
+function dots29down(){
+    dots29.style.opacity = '0'
+}
+
+function dots29purple(){
+
+    dots29.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots29.style.color = "white";
+}
+
+function dots29white(){
+    dots29.style.backgroundColor = "white";
+    dots29.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots30up(){
+    dots30.style.opacity = '1'
+
+}
+
+function dots30down(){
+    dots30.style.opacity = '0'
+}
+
+function dots30purple(){
+
+    dots30.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots30.style.color = "white";
+}
+
+function dots30white(){
+    dots30.style.backgroundColor = "white";
+    dots30.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots31up(){
+    dots31.style.opacity = '1'
+
+}
+
+function dots31down(){
+    dots31.style.opacity = '0'
+}
+
+function dots31purple(){
+
+    dots31.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots31.style.color = "white";
+}
+
+function dots31white(){
+    dots31.style.backgroundColor = "white";
+    dots31.style.color = 'rgba(84,84,85,255)';
+}
+
+function dots32up(){
+    dots32.style.opacity = '1'
+
+}
+
+function dots32down(){
+    dots32.style.opacity = '0'
+}
+
+function dots32purple(){
+
+    dots32.style.backgroundColor = 'rgba(115, 1, 231, 255)'
+    dots32.style.color = "white";
+}
+
+function dots32white(){
+    dots32.style.backgroundColor = "white";
+    dots32.style.color = 'rgba(84,84,85,255)';
+}
+
+
+
+
+
+
+
+
+
+ $('.dots-normal').bind('click', function(e) {
+
+        e.preventDefault();
+
+        
+
+        $("#context-menu").css({
+            top: e.pageY,
+            left: e.pageX
+        }).fadeIn(200, function(){
+            $(document).on('click', function(){
+                $("#context-menu").hide();
+                $(document).off('click');
+            })
+        });
+
+
+
+
+
+
+
+    })
+
+
+$('.dots-edge').bind('click', function(e) {
+
+        e.preventDefault();
+        $("#context-menu").css({
+            top: e.pageY,
+            left: "65rem"
+        }).fadeIn(200, function(){
+            $(document).on('click', function(){
+                $("#context-menu").hide();
+                $(document).off('click');
+            })
+        });
+
+    })
